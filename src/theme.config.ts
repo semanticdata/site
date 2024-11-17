fadffa77bcf8b97d4e767677e55b2d20c9936166
semanticdata <contact@miguelpimentel.do>
2024-11-17 09:54:33 -0600

    change base url

--- src/theme.config.ts
+++ src/theme.config.ts
@@ -1,10 +1,10 @@
 import { defineThemeConfig } from './types'
 
 export default defineThemeConfig({
-  site: 'https://nordlys.fjelloverflow.dev',
-  title: 'Nordlys',
-  description: 'A minimal Astro blog theme',
-  author: 'FjellOverflow',
+  site: 'https://semanticdata.github.io/site/',
+  title: 'Nordlys Site',
+  description: 'A minimal Astro blog site.',
+  author: 'Miguel Pimentel',
   navbarItems: [
     { label: 'Blog', href: '/posts' },
     { label: 'Projects', href: '/projects' },
@@ -23,7 +23,7 @@ export default defineThemeConfig({
   footerItems: [
     {
       icon: 'tabler--brand-github',
-      href: 'https://github.com/FjellOverflow/nordlys',
+      href: 'https://github.com/semanticdata/site',
       label: 'Github'
     },
     {
